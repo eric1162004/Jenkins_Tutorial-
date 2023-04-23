16ef44b00386c87007ef15022ba72300ae3f4ad8
Eric Cheung <eric1162004@hotmail.com>
2023-04-23 15:27:21 -0700

    demo 1

---
+++ Pipelines/1-demo.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing'
+                // sh 'exit 1'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying'
+            }
+        }
+    }
+}
